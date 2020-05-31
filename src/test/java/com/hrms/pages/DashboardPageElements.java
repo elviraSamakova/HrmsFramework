2c323605eff02bd3133c231fc5e268e3709552fc
elviraSamakova <elvira.samakova@gmail.com>
2020-05-31 00:54:43 -0400

    first class of framework, installed and coppied to neww HRMSclass

---
+++ src/test/java/com/hrms/pages/DashboardPageElements.java
@@ -0,0 +1,73 @@
+package com.hrms.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.hrms.testbase.BaseClass;
+import com.hrms.utils.CommonMethods;
+
+public class DashboardPageElements extends CommonMethods {
+	
+	/*
+	 * @FindBy(id = "menu_pim_viewPimModule")
+	public WebElement pim;
+	@FindBy(id = "menu_pim_addEmployee")
+	public WebElement addEmployee;
+	 */
+
+	@FindBy(id = "welcome")
+	public WebElement welcome;
+	
+	@FindBy(xpath = "//div[@id='branding']/a[1]/img")
+	public WebElement logo;
+
+	@FindBy(id = "menu_pim_addEmployee")
+	public WebElement addEmployee;
+	
+	//@FindBy(xpath = "//a[@id='menu_pim_addEmployee']")
+	//public WebElement addEmployee;
+	
+	@FindBy(id = "menu_pim_viewPimModule")
+	public WebElement pim;
+	
+	@FindBy(id = "menu_recruitment_viewRecruitmentModule")
+	public WebElement recruitment;
+	
+	@FindBy (id= "menu_recruitment_viewCandidates")
+	public WebElement candidates;
+	
+
+	public DashboardPageElements() {
+		PageFactory.initElements(BaseClass.driver, this);
+	}
+	
+	public void navigateToAddEmployee() {
+		click(pim);
+		click(addEmployee);
+	}
+	
+	public void navigateToCandidates() {
+		click(recruitment);
+		click(candidates);
+	}
+	
+	public void hoverOverCandidates() {
+		Actions action= new Actions(driver);
+		action.moveToElement(recruitment).perform();
+		click(candidates);
+	}
+
+	public void hoverOverAddEmployee() {
+		Actions action= new Actions(driver);
+		action.moveToElement(pim).perform();
+		click(addEmployee);
+	}
+
+	
+	
+	
+	
+	
+}
---
+++ src/test/java/com/hrms/testbase/BaseClass.java
@@ -0,0 +1,56 @@
+package com.hrms.testbase;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import com.hrms.utils.ConfigsReader;
+import com.hrms.utils.Constants;
+
+public class BaseClass {
+	public static WebDriver driver;
+
+	@BeforeMethod(alwaysRun = true)
+	public static WebDriver setUp() {
+
+		ConfigsReader.readProperties(Constants.CONFIGURATION_FILEPATH);
+
+		System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "true");
+
+		switch (ConfigsReader.getProperty("browser").toLowerCase()) {
+
+		case "chrome":
+			System.setProperty("webdriver.chrome.driver", Constants.CHROME_DRIVER_PATH);
+			driver = new ChromeDriver();
+			break;
+		case "firefox":
+			System.setProperty("webdriver.gecko.driver", Constants.GECKO_DRIVER_PATH);
+			driver = new FirefoxDriver();
+			break;
+
+		default:
+			throw new RuntimeException("Browser is not supported");
+		}
+
+		// driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Constants.IMPLICIT_WAIT_TIME, TimeUnit.SECONDS);
+
+		driver.get(ConfigsReader.getProperty("url"));
+		// initialize all page objects as part of setup
+		PageInitializer.initialize();
+
+		return driver;
+	}
+
+	 @AfterMethod (alwaysRun = true)
+	public static void tearDown() {
+		if (driver != null) {
+			driver.quit();
+		}
+	}
+}
